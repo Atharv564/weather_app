c64a0d999fb222b17b67915022decceebaa3cc24
Atharv <atharvingale564@gmail.com>
2025-02-15 18:16:32 +0530

    commit

---
+++ weather_project/static/js/service-worker.js
@@ -0,0 +1,12 @@
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open("weather-cache").then((cache) => {
+            return cache.addAll(["/"]);
+        })
+    );
+});
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => response || fetch(event.request))
+    );
+});
---
+++ weather_project/weather/views.py
@@ -0,0 +1,19 @@
+import requests
+from django.shortcuts import render
+
+def weather_view(request):
+    api_key = "7b318d7c174e441c86c114241251502"  # Replace with your WeatherAPI key
+    city = request.GET.get('city', 'New York')  # Default city
+
+    url = f"http://api.weatherapi.com/v1/forecast.json?key={api_key}&q={city}&days=5&aqi=no&alerts=no"
+    response = requests.get(url).json()
+
+    weather_data = {
+        'city': city,
+        'temperature': response.get('current', {}).get('temp_c', 'N/A'),
+        'condition': response.get('current', {}).get('condition', {}).get('text', 'N/A'),
+        'icon': response.get('current', {}).get('condition', {}).get('icon', ''),
+        'forecast': response.get('forecast', {}).get('forecastday', [])  # 5-day forecast
+    }
+
+    return render(request, 'weather/weather.html', {'weather': weather_data})
